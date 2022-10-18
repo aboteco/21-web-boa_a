cde86453eddd9f8703ec6a3a7953cd84a8321ccf
aboteco <115123847+aboteco@users.noreply.github.com>
2022-10-18 20:09:18 -0300

    Add files via upload

---
+++ hola.js
@@ -0,0 +1,16 @@
+/// ejercicio 1 var nombre="pedrito"
+/// ejercicio 1 var nombre1="rufo"
+/// ejercicio 1 var nombre2="chancla"
+/// ejercicio 1  console.log ("mis mascotas son",nombre,nombre1,nombre2)
+
+
+ /// ejercicio 2 var mascota1=(prompt("dime el nombre de tu mascota"))
+ /// ejercicio 2 var mascota2=(prompt("dime el nombre de tu otra mascota"))
+ /// ejercicio 2 console.log("quiero mucho a mis mascotas", mascota1,mascota2)
+
+var cadena="100"
+console.log(cadena)
+console.log(typeof cadena)
+var cadena=100
+console.log(cadena)
+console.log(typeof cadena)
